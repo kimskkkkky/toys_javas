539aefc8f495c1ba339c6ac4c9335dfaf1a14d46
kimskkkkky <132973456+kimskkkkky@users.noreply.github.com>
2023-05-31 22:02:12 +0900

    1차 저장

--- src/PollsWithMethod.java
+++ src/PollsWithMethod.java
@@ -1,5 +1,13 @@
+import polls.PollInitailArrays;
 public class PollsWithMethod {
     public static void main(String[] args) {
-        
+        try {
+            PollInitailArrays pollInitailArrays = new PollInitailArrays();
+            
+            
+            
+             } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
 }
