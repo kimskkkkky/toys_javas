47f71f85c26582055dfa8ff827716f38c813fd83
kimskkkkky <132973456+kimskkkkky@users.noreply.github.com>
2023-05-31 22:34:29 +0900

    초안

--- src/PollsWithMethod.java
+++ src/PollsWithMethod.java
@@ -3,6 +3,10 @@ public class PollsWithMethod {
     public static void main(String[] args) {
         try {
             PollInitailArrays pollInitailArrays = new PollInitailArrays();
+            String[][] survey = pollInitailArrays.initialArrays();
+            for (int num = 0;  polls.length > num; num = num+1) {
+            System.out.println(polls[num]);        
+        }
             
             
             
