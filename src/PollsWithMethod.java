7910703f31a806b971b35b0e1265c7b8294b68a8
kimskkkkky <132973456+kimskkkkky@users.noreply.github.com>
2023-06-01 00:03:29 +0900

    뭐야 이게....

--- src/PollsWithMethod.java
+++ src/PollsWithMethod.java
@@ -1,13 +1,20 @@
-import polls.PollInitailArrays;
+import polls.PollInitailArrays; //초기화 및 질문, 보기 불러오기 경로
+import polls.PollScanners;  //답변 입력을 위한 경로
 public class PollsWithMethod {
     public static void main(String[] args) {
         try {
-            PollInitailArrays pollInitailArrays = new PollInitailArrays();
+            Scanner myObj = new Scanner(System.in);
+            PollInitailArrays pollInitailArrays = new PollInitailArrays();  //질문, 보기 출력
             String[][] survey = pollInitailArrays.initialArrays();
             for (int num = 0;  survey.length > num; num = num+1) {
             System.out.println(survey[num][0]);        
         }
-            
+            PollScanners pollScanners = new PollScanners();  //답변 스캐너로 출력
+            int [] answer = pollScanners.Arrays();
+            int count = 0;
+            System.out.print("답)" + "");
+            answer[count] = myObj.nextLine();
+            count = count + 1;
             
             
              } catch (Exception e) {
