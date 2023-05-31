c8472d1220a9b3d85d353dd96d2198e9c3a02bca
kimskkkkky <ses513a@gmail.com>
2023-05-31 18:07:23 +0900

    공유

---
+++ src/PollInitailArrays.java
@@ -0,0 +1,6 @@
+public class PollInitailArrays
+public static void main(String[] args) {
+    
+} {
+    
+}
---
+++ src/PollStatistics.java
@@ -0,0 +1,5 @@
+public class PollStatistics {
+    public static void main(String[] args) {
+        
+    }
+}
