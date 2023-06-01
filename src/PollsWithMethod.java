d2082e22d321292068a1765058b0f0521aa6a835
kimskkkkky <ses513a@gmail.com>
2023-06-01 10:54:24 +0900

    통계 마무리 해라

--- src/PollsWithMethod.java
+++ src/PollsWithMethod.java
@@ -3,18 +3,16 @@ import polls.PollScanners;  //답변 입력을 위한 경로
 public class PollsWithMethod {
     public static void main(String[] args) {
         try {
-            Scanner myObj = new Scanner(System.in);
+
             PollInitailArrays pollInitailArrays = new PollInitailArrays();  //질문, 보기 출력
-            String[][] survey = pollInitailArrays.initialArrays();
-            for (int num = 0;  survey.length > num; num = num+1) {
-            System.out.println(survey[num][0]);        
-        }
+            String[][] polls = pollInitailArrays.initialArrays();
+
             PollScanners pollScanners = new PollScanners();  //답변 스캐너로 출력
-            int [] answer = pollScanners.Arrays();
-            int count = 0;
-            System.out.print("답)" + "");
-            answer[count] = myObj.nextLine();
-            count = count + 1;
+            int[] Answer = pollScanners.Arrays();
+            // int count = 0;
+            // System.out.print("답)" + "");
+            // answer[count] = myObj.nextLine();
+            // count = count + 1;
             
             
              } catch (Exception e) {
--- src/polls/PollScanners.java
+++ src/polls/PollScanners.java
@@ -4,6 +4,9 @@ public class PollScanners {
     public int[] Arrays(){
         int[] Answer = new int[4];
         Scanner myObj = new Scanner(System.in);
+        int answer = 0;
+         Answer[answer] = myObj.nextInt();
+         answer = answer+1;
 
         return Answer;
     }
