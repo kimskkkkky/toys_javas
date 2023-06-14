64941a14e10b6682bb2519501908bb481496cdc0
kimskkkkky <ses513a@gmail.com>
2023-06-14 11:02:50 +0900

    설문가능자 리스트

--- src/PollsWithDB.java
+++ src/PollsWithDB.java
@@ -20,21 +20,28 @@ public class PollsWithDB {
          String workKey = "A";
          Statement statement = connection.createStatement();
          String query = "";
+         System.out.print("선택 입력 : ");
          // E를 제외한 나머지 경우
-         while (!workKey.equals("E")) {
-            System.out.println("선택 입력 : ");
+         while (!workKey.equals("E")) {  //E를 누르면 설문 종료
             workKey = scanner.nextLine();
-
             // P를 누른 경우
-            if (workKey.equals("P")) {
+            if (workKey.equals("P")) {   // P를 입력하면 설문가능 명단 띄우기
                System.out.println("- 설문자 가능 명단(가입 완료)");
-               int number = 1;
+               int number = 1; //설문자 입력 번호
                query = "SELECT `user`.`USER`\n" + //
                      "FROM `user`\n";
                ResultSet resultSet = statement.executeQuery(query);
                 while (resultSet.next()) {
                System.out.println(number+ "." + resultSet.getString("USER"));
                 number = number + 1;}
+                System.out.print("- 설문자 번호 입력 : ");  // 설문자 번호 입력
+               number = scanner.nextInt(); //해당 설문자 번호 입력
+               if(number > 4) //최대 4명이니깐 4 이상 숫자 입력시 Error 메시지 출력
+               {
+               System.out.println("-Error- 확인 후 입력 필요 ");
+               }else {
+               System.out.println("-- 설문시작 -- ");
+               }
                // S를 누른 경우
             } else if (workKey.equals("S")) {
                System.out.println("설문 조사 통계");
